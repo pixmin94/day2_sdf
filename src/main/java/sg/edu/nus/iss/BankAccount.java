e725f7a533d7172cc0629d8c570abac35c04495d
pixmin94 <pixmin@outlook.com>
2023-04-18 19:47:45 +0800

    Set up java application

---
+++ src/main/java/sg/edu/nus/iss/BankAccount.java
@@ -0,0 +1,15 @@
+package sg.edu.nus.iss;
+
+import java.util.Date;
+import java.util.List;
+
+public class BankAccount {
+    private final String fullName;
+    private String accountNumber;
+    private float balance;
+    private List<String> transactions;
+    private boolean isClosed;
+    private Date accountStartDate;
+    private Date accountEndDate;
+    
+}
